386c2d161db70ff4c0c4213157b0192864e4ce15
Pranay Khadse <pranitkhadse20@gmail.com>
2023-02-17 23:30:36 +0530

    backend

---
+++ server.js
@@ -0,0 +1,11 @@
+let jsonServer = require('json-server')
+let Server = jsonServer.create()
+let Router = jsonServer.router('./db.json')
+let Middleware = jsonServer.defaults()
+let cors = require('cors')
+
+Server.use(cors())
+Server.use(Router)
+Server.use(Middleware)
+
+Server.listen(8000)
